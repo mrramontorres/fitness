1722e3cb9ff9b6d047d0dde9580f7d2f48979a37
turtles <me@ramon.nyc>
2021-03-10 22:27:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+// Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
@@ -14,10 +15,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes
-app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes.js"));
+// Routes
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
 
+// Listening
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
