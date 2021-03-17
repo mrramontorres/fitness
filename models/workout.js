3573a46bf292637b135d436bd319e47c01899883
turtles <me@ramon.nyc>
2021-03-16 20:44:22 -0700

    Update workout.js

--- models/workout.js
+++ models/workout.js
@@ -9,9 +9,30 @@ const workoutSchema = new Schema({
         },
         exercise: [
             {
+              type:{
                 type: String,
                 trim: true,
                 required: "Enter an exercise type"
+              },
+              name:{
+                type: String,
+                trim: true,
+                required: "Enter an exercise name"
+              },
+              duration:{
+                type: String,
+                trim: true,
+                required: "Enter an exercise duration"
+              },
+              weight:{
+                type: Number
+              },
+              reps:{
+                type: Number
+              },
+              sets:{
+                type: Number
+              },
             },
         ]
 
