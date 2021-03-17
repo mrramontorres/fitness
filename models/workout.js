4659cbf1a11bf402547692d41795b72581d0e145
turtles <me@ramon.nyc>
2021-03-16 20:54:14 -0700

    Update workout.js

--- models/workout.js
+++ models/workout.js
@@ -3,37 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-        day: {
-            type: Date,
-            default: Date.now
-        },
-        exercise: [
-            {
-              type:{
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercise: [
+        {
+            type:{
                 type: String,
                 trim: true,
                 required: "Enter an exercise type"
-              },
-              name:{
+            },
+            name:{
                 type: String,
                 trim: true,
                 required: "Enter an exercise name"
-              },
-              duration:{
+            },
+            duration:{
                 type: String,
                 trim: true,
                 required: "Enter an exercise duration"
-              },
-              weight:{
-                type: Number
-              },
-              reps:{
-                type: Number
-              },
-              sets:{
-                type: Number
-              },
             },
-        ]
+            weight:{
+                type: Number
+            },
+            reps:{
+                type: Number
+            },
+            sets:{
+                type: Number
+            }
+        }
+    ]
+});
 
-    })
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
