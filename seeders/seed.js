84e0c03ec15db407c1f6baa9fdcb369e2a131727
turtles <me@ramon.nyc>
2021-03-17 22:59:00 -0700

    fixed local host or statment

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models/workout.js");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/forest", 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
