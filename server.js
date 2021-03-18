0f5330d63bbcfec89a74d5e85e0a237ca1ec02ce
turtles <me@ramon.nyc>
2021-03-17 22:44:15 -0700

    Update server.js
    connected to mongoDB

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/forest", 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // Routes
 require("./routes/api-routes.js")(app);
